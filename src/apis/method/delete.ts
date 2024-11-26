e4441abcb9594335486e47a834c2016083b68a5f
minhoshin11 <tlsalsgh777@naver.com>
2024-11-26 15:07:53 +0900

    MNM-21-feat: api 메소드 기능구현

--- src/apis/method/delete.ts
+++ src/apis/method/delete.ts
@@ -1,13 +1,13 @@
 export async function deleteData(url: string, headers?: HeadersInit) {
-    const response = await fetch(url, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-            ...headers,
-        },
-    });
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-    }
-    return await response.json();
+  const response = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+  return response.json();
 }
--- src/apis/method/get.ts
+++ src/apis/method/get.ts
@@ -1,22 +1,19 @@
 //GET
 export async function getData(url: string, headers?: HeadersInit) {
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            ...headers,
-        },
-    });
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-    }
-    const contentType = response.headers.get('Content-Type') || '';
-    if (contentType.includes('application/json')) {
-        return await response.json(); // JSON 변환
-    } else {
-        return await response.text(); // JSON이 아니면 그냥 텍스트 반환
-    }
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status}`);
+  }
+  const contentType = response.headers.get("Content-Type") || "";
+  if (contentType.includes("application/json")) {
+    return response.json(); // JSON 변환
+  } else {
+    return response.text(); // JSON이 아니면 그냥 텍스트 반환
+  }
 }
-
-
-
