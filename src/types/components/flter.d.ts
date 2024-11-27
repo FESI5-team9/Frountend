10a19d414bb0a7d09c50b16c5e00ba82157e95ed
[AI6/1반] 손유경 <ykson94@gmail.com>
2024-11-27 11:39:17 +0900

    MNM-23-refactor: 불필요한 type 삭제

--- src/types/components/flter.d.ts
+++ src/types/components/flter.d.ts
@@ -1,11 +1,5 @@
 type FilterType = "sortFilter" | "selectionFilter";
 
-type FilterProps = {
-  filterType: FilterType;
-  selectedOption: string;
-  style: string;
-};
-
 type DropDownProps = {
   filterType: FilterType;
   options: string[];
