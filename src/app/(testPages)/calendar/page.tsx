93005675b0f8a88a032986ca17899d42ec21886a
[AI6/1반] 손유경 <ykson94@gmail.com>
2024-11-29 09:21:35 +0900

    MNM-35-refactor: 공통 컴포넌트 dropdown, calendar 리팩토링

--- src/app/(testPages)/calendar/page.tsx
+++ src/app/(testPages)/calendar/page.tsx
@@ -6,13 +6,13 @@ import Calendar from "@/components/calendar/Calendar";
 export default function Page() {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const getSelectedDate = (date: Date) => {
+  const handleDateSelect = (date: Date) => {
     setSelectedDate(date);
   };
 
   return (
     <div className="h-screen bg-slate-400 pt-20">
-      <Calendar getSelectedDate={getSelectedDate} />
+      <Calendar handleDateSelect={handleDateSelect} />
       <p className="mt-48">선택된 날짜: {selectedDate.toLocaleDateString()}</p>
     </div>
   );
