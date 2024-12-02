c16b5adcaab33b5f1af260034b14086bf1010cb2
JooKangSan <you04045@naver.com>
2024-12-02 14:01:45 +0900

    MNM-46-feat: 테스트 코드 작성

--- src/apis/reviewsApi.ts
+++ src/apis/reviewsApi.ts
@@ -9,10 +9,41 @@ import {
 import fetchInstance from "./fetchInstance";
 
 // 리뷰 목록 조회
-export async function getReviews(params: GetReviews) {
-  const data = await fetchInstance.get<ReviewsRes>(
-    `/reviews?gatheringId=${params.gatheringId}&userId=${params.userId}&type=${params.type}&location=${params.location}&date=${params.date}&registrationEnd=${params.registrationEnd}&size=${params.size}&page=${params.page}&sort=${params.sort}&direction=${params.direction}`,
-  );
+export async function getReviews(params?: GetReviews) {
+  const query = new URLSearchParams();
+
+  if (params?.gatheringId) {
+    query.set("gatheringId", params.gatheringId.toString());
+  }
+  if (params?.userId) {
+    query.set("userId", params.userId.toString());
+  }
+  if (params?.type) {
+    query.set("type", params.type);
+  }
+  if (params?.location) {
+    query.set("location", params.location);
+  }
+  if (params?.date) {
+    query.set("date", params.date);
+  }
+  if (params?.registrationEnd) {
+    query.set("registrationEnd", params.registrationEnd);
+  }
+  if (params?.size) {
+    query.set("size", params.size.toString());
+  }
+  if (params?.page) {
+    query.set("page", params.page.toString());
+  }
+  if (params?.sort) {
+    query.set("sort", params.sort);
+  }
+  if (params?.direction) {
+    query.set("direction", params.direction);
+  }
+
+  const data = await fetchInstance.get<ReviewsRes>(`/reviews?${query.toString()}`);
   return data;
 }
 
@@ -24,8 +55,15 @@ export async function addReviews(body: AddReviews) {
 
 // 리뷰 평점 목록 조회
 export async function getReviewsRating(params: GetReviewsRating) {
-  const data = await fetchInstance.get<GetReviewsRatingRes>(
-    `/reviews/score?gatherings?gatheringId=${params.gatheringId}&type=${params.type}`,
-  );
+  const query = new URLSearchParams();
+
+  if (params?.gatheringId) {
+    query.set("gatheringId", params.gatheringId.toString());
+  }
+  if (params?.type) {
+    query.set("type", params.type);
+  }
+
+  const data = await fetchInstance.get<GetReviewsRatingRes>(`/reviews/score?${query.toString()}`);
   return data;
 }
