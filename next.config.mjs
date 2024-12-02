d20d17f5651b340bc87b3101382a2ea3419c06d5
JooKangSan <you04045@naver.com>
2024-12-02 09:25:33 +0900

    MNM-42-refactor: env 파일 수정 및 config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "http://15.165.237.161:8080/api/:path*",
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
       },
     ];
   },
