9f8f0b178a53178ba85080324f43c5887f72913c
JooKangSan <you04045@naver.com>
2024-12-02 22:10:46 +0900

    MNM-47-feat: intercepter에 토큰 기능추가

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -6,13 +6,11 @@ export async function setAuthCookies(accessToken: string, refreshToken: string) 
   const cookieStore = cookies();
 
   cookieStore.set("accessToken", accessToken, {
-    httpOnly: true,
     secure: true,
     sameSite: "strict",
   });
 
   cookieStore.set("refreshToken", refreshToken, {
-    httpOnly: true,
     secure: true,
     sameSite: "strict",
   });
