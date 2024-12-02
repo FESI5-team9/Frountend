8963809bebe8e4c75dd5264dd83e27f38cf0bd85
JooKangSan <you04045@naver.com>
2024-12-02 09:28:56 +0900

    MNM-45-refactor: next.config  수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "http://13.209.31.208:8080/api/:path*",
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/api/:path*`,
       },
     ];
   },
