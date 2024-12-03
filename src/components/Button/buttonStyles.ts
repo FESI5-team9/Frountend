ede52ab6ebc9ef192be38301e1e919bcacf6c5f6
JooKangSan <you04045@naver.com>
2024-12-03 10:49:40 +0900

    MNM-57-refactor: hover 추가

--- src/components/Button/buttonStyles.ts
+++ src/components/Button/buttonStyles.ts
@@ -1,28 +1,29 @@
 export const getFilledStyle = (color: string, disabled?: boolean) => {
   if (disabled) return "bg-gray-400 text-white cursor-not-allowed";
+
   switch (color) {
     case "yellow":
-      return "bg-yellow-primary text-gray-800";
+      return "bg-yellow-primary text-gray-800 hover:bg-[#FFDB38]";
     case "orange":
-      return "bg-orange-primary text-white";
+      return "bg-orange-primary text-white hover:bg-[#F28B35]";
     case "red":
-      return "bg-red-primary text-white";
+      return "bg-red-primary text-white hover:bg-[#FF4122]";
     case "disabled":
-      return "bg-gray-400 text-white";
+      return "bg-gray-400 text-white cursor-not-allowed";
   }
 };
-
 export const getOutlinedStyle = (color: string, disabled?: boolean) => {
   if (disabled) return "bg-white text-gray-400 border border-gray-400 cursor-not-allowed";
+
   switch (color) {
     case "yellow":
-      return "bg-white text-yellow-primary border border-yellow-primary";
+      return "bg-white text-yellow-primary border border-yellow-primary hover:bg-yellow-100";
     case "orange":
-      return "bg-white text-orange-primary border border-orange-primary";
+      return "bg-white text-orange-primary border border-orange-primary hover:bg-orange-100";
     case "red":
-      return "bg-white text-red-primary border border-red-primary";
+      return "bg-white text-red-primary border border-red-primary hover:bg-red-100";
     case "disabled":
-      return "bg-white text-gray-400 border border-gray-400 cursor-not-allowed";
+      return "bg-white text-gray-400 border border-gray-400 cursor-not-allowed hover:bg-gray-100";
   }
 };
 
