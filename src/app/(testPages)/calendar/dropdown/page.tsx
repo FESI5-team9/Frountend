6beab756245104f569225a0f3f17d84047c85215
JooKangSan <you04045@naver.com>
2024-12-05 10:42:32 +0900

    MNM-35-chore: build 를 위해 임시 주석 처리

--- src/app/(testPages)/calendar/dropdown/page.tsx
+++ src/app/(testPages)/calendar/dropdown/page.tsx
@@ -1,17 +1,17 @@
-// "use client";
+"use client";
 
 // import DropdownCalendar from "@/components/Calendar/DropdownCalendar";
-// import useDateStore from "@/store/dateStore";
+import useDateStore from "@/store/dateStore";
 
-// export default function Page() {
-//   const { firstDate } = useDateStore();
+export default function Page() {
+  const { firstDate } = useDateStore();
 
-//   return (
-//     <div className="h-screen bg-slate-400">
-//       <DropdownCalendar />
-//       <p className="mt-[400px]">
-//         선택된 날짜: {firstDate ? firstDate.toLocaleDateString() : "날짜가 선택되지 않았습니다"}
-//       </p>
-//     </div>
-//   );
-// }
+  return (
+    <div className="h-screen bg-slate-400">
+      {/* <DropdownCalendar /> */}
+      <p className="mt-[400px]">
+        선택된 날짜: {firstDate ? firstDate.toLocaleDateString() : "날짜가 선택되지 않았습니다"}
+      </p>
+    </div>
+  );
+}
