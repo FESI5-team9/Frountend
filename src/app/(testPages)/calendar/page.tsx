cb029674504b6ef651cc08bd16e522fe11d0f1ff
JooKangSan <you04045@naver.com>
2024-12-05 10:38:14 +0900

    MNM-35-chore: build 를 위해 임시 주석 처리

--- src/app/(testPages)/calendar/page.tsx
+++ src/app/(testPages)/calendar/page.tsx
@@ -1,19 +1,19 @@
-"use client";
+// "use client";
 
-import Calendar from "@/components/Calendar/Calendar";
-import useDateStore from "@/store/dateStore";
+// import Calendar from "@/components/Calendar/Calendar";
+// import useDateStore from "@/store/dateStore";
 
-export default function Page() {
-  const { firstDate, secondDate } = useDateStore();
+// export default function Page() {
+//   const { firstDate, secondDate } = useDateStore();
 
-  return (
-    <div className="flex h-screen w-full flex-col items-center bg-slate-400 pt-20">
-      <Calendar multipleDates={true} />
+//   return (
+//     <div className="flex h-screen w-full flex-col items-center bg-slate-400 pt-20">
+//       <Calendar multipleDates={true} />
 
-      <div className="">
-        <p className="mt-20">선택된 날짜: {firstDate ? firstDate.toLocaleDateString() : "없음"}</p>
-        <p className="">선택된 날짜: {secondDate ? secondDate.toLocaleDateString() : "없음"}</p>
-      </div>
-    </div>
-  );
-}
+//       <div className="">
+//         <p className="mt-20">선택된 날짜: {firstDate ? firstDate.toLocaleDateString() : "없음"}</p>
+//         <p className="">선택된 날짜: {secondDate ? secondDate.toLocaleDateString() : "없음"}</p>
+//       </div>
+//     </div>
+//   );
+// }
