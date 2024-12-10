72d07d5aec36964c7d0664f4c2ef222f6067af63
JooKangSan <you04045@naver.com>
2024-12-10 10:58:18 +0900

    MNM-68-feat: react query 추가

---
+++ src/app/providers.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider, isServer } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000, // 데이터 보관 기간
+        gcTime: 15 * 60 * 1000, // 사용하지 않은 데이터 보관 기간
+        refetchOnWindowFocus: false, // 이거 다른페이지 왓다가 다시 오면 페칭해서 연산 많이먹는거 막았음.
+      },
+    },
+  });
+}
+let browserQueryClient: QueryClient | undefined;
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  }
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+}
+export default function Providers({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
