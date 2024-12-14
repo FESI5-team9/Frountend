f29c6325954fc062c42b2739e50eae44b01f3edc
ohjohwan <whghks1020@naver.com>
2024-12-14 23:42:44 +0900

    MNM-81-feat: GNB 프로필 클릭 시 마이페이지, 로그아웃 기능 구현

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import useUserStore from "@/store/userStore";
 
 export default function Gnb() {
-  const { id, image } = useUserStore();
+  const { id, image, setUser } = useUserStore();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleLogout = () => {
+    setUser({
+      id: null,
+      email: null,
+      nickname: null,
+      image: null,
+    });
+
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+
+    window.location.href = "/";
+  };
 
   return (
     <header>
@@ -32,24 +48,47 @@ export default function Gnb() {
             </div>
           </div>
 
-          <div className="flex items-center gap-5 tablet:gap-6">
-            <Link href={`/search/${id}`}>
-              <Image src="/icons/magnifier.svg" width={24} height={24} alt="검색" />
-            </Link>
-            {id ? (
-              // 로그인 상태면 유저 프로필
-              <button className="" aria-label="유저 프로필">
-                <Image src={image || "/images/profile.svg"} width={40} height={40} alt="프로필" />
-              </button>
-            ) : (
-              // 비로그인 상태면 로그인 링크
-              <Link
-                href={"/signin"}
-                className="text-sm font-semibold hover:text-white tablet:text-base"
-                aria-label="로그인"
-              >
-                로그인
+          <div className="relative flex flex-col gap-2">
+            <div className="flex items-center gap-5 tablet:gap-6">
+              <Link href={`/search/${id}`}>
+                <Image src="/icons/magnifier.svg" width={24} height={24} alt="검색" />
               </Link>
+              {id ? (
+                // 로그인 상태면 유저 프로필
+                <button
+                  className=""
+                  aria-label="유저 프로필"
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                  <Image src={image || "/images/profile.svg"} width={40} height={40} alt="프로필" />
+                </button>
+              ) : (
+                // 비로그인 상태면 로그인 링크
+                <Link
+                  href={"/signin"}
+                  className="text-sm font-semibold hover:text-white tablet:text-base"
+                  aria-label="로그인"
+                >
+                  로그인
+                </Link>
+              )}
+            </div>
+            {isMenuOpen && (
+              <div className="absolute right-1 top-[50px] flex h-[80px] w-[110px] flex-col justify-start rounded-2xl bg-white shadow-lg desktop:h-[88px] desktop:w-[142px]">
+                <Link
+                  href={"/mypage"}
+                  className="inline-block h-10 py-[10px] pl-3 text-sm tablet:h-11 tablet:pl-4 tablet:text-base"
+                >
+                  마이페이지
+                </Link>
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="h-10 py-[10px] pl-3 text-left text-sm tablet:h-11 tablet:pl-4 tablet:text-base"
+                >
+                  로그아웃
+                </button>
+              </div>
             )}
           </div>
         </div>
