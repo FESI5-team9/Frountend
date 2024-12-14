fd8a2b90481c244eb14ea93e67fb6ac3632d5cb2
[AI6/1반] 손유경 <ykson94@gmail.com>
2024-12-14 22:53:21 +0900

    MNM-41-feat: 상세페이지 params.id 유효성 검증 추가

--- src/app/groupDetail/[id]/page.tsx
+++ src/app/groupDetail/[id]/page.tsx
@@ -5,8 +5,16 @@ import GroupDetail from "../_components/GroupDetail";
 async function GroupDetailPage({ params }: { params: { id: string } }) {
   const queryClient = new QueryClient();
 
+  if (!params.id || isNaN(Number(params.id))) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <p>Invalid request. ID is required.</p>
+      </div>
+    );
+  }
+
   await queryClient.prefetchQuery({
-    queryKey: ["posts", params.id],
+    queryKey: ["gatheringDetail", params.id],
     queryFn: () => getGatheringDetail(Number(params.id)),
   });
 
