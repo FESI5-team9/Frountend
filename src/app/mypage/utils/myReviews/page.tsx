98a3fffc1af689b6d8641d5f85c0172008c00b9b
ohjohwan <whghks1020@naver.com>
2024-12-15 00:29:08 +0900

    MNM-40-refactor: myReviews 불필요한  파일 삭제

--- src/app/mypage/utils/myReviews/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function myReviews() {
-  return <div>나의 리뷰</div>;
-}
--- src/app/mypage/utils/myReviews/review/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function myReviews() {
-  return (
-    <>
-      <div>나의 리뷰</div>
-      <div>콘텐츠</div>
-    </>
-  );
-}
