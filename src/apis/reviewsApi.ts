e16de90c2547b023708687f1aa0bd645d69fdb61
JooKangSan <you04045@naver.com>
2024-12-17 20:56:04 +0900

    MNM-84-refactor: 리뷰목록조회, 추가 리팩토링

--- src/apis/reviewsApi.ts
+++ src/apis/reviewsApi.ts
@@ -9,21 +9,34 @@ import {
   ReviewsRes,
 } from "@/types/api/reviews";
 import fetchInstance from "./fetchInstance";
+import { fetchWithMiddleware } from "./fetchWithMiddleware";
 
 // 리뷰 목록 조회
 export async function getReviews(params?: GetReviews) {
   const searchParams = new URLSearchParams();
-  const queryString = params ? buildQueryParams(searchParams, params) : "";
-
-  const data = await fetchInstance.get<ReviewsRes>(
-    `/reviews${queryString ? `?${queryString}` : ""}`,
-  );
+  if (params) {
+    buildQueryParams(searchParams, params);
+  }
+  const response = await fetchWithMiddleware(`/api/reviews?${searchParams.toString()}`, {
+    method: "GET", // GET 요청
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data: ReviewsRes = await response.json();
   return data;
 }
 
 // 리뷰 추가
 export async function addReviews(body: AddReviews) {
-  const data = await fetchInstance.post<AddReviewsRes>("/reviews", body);
+  const response = await fetchWithMiddleware("/reviews", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const data: AddReviewsRes = await response.json();
   return data;
 }
 
@@ -37,6 +50,7 @@ export async function getReviewsRating(params: GetReviewsRating) {
   );
   return data;
 }
+
 // 타입 별 리뷰 평점 통계 조회
 export async function getReviewStats(type: string) {
   const data = await fetchInstance.get<GetReviewStatsRes>(
