1b6eebbc127078e58b7c177b144dd12531f1bfa8
JooKangSan <you04045@naver.com>
2024-12-17 23:01:55 +0900

    MNM-84-feat: route 생성

---
+++ src/app/api/my/gatherings/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const params = url.searchParams;
+
+  const queryString = new URLSearchParams(params).toString();
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/my/gatherings?${queryString}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${request.cookies.get("access-token")?.value}`,
+    },
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
+  } else {
+    const errorData = await response.json();
+    return NextResponse.json(errorData, { status: response.status });
+  }
+}
