15ecf3fd1ea4007a738c7c70de510b51decff7a5
JooKangSan <you04045@naver.com>
2024-12-18 16:31:04 +0900

    MNM-93-refactor: 로그인 및 회원가입 페이지 리팩토링

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,27 +7,57 @@ function SignRootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDRICT_URL}&response_type=code`;
+  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDRICT_URL}&response_type=code`;
   return (
-    <div className="relative min-h-screen w-full bg-gray-100">
+    <div className="w-ful relative min-h-screen bg-white">
       <Gnb />
-      <div className="mx-auto flex h-full min-h-screen w-full flex-col items-center justify-center gap-12 px-4 py-10 pt-[88px] tablet:px-[75px] desktop:flex-row desktop:px-4">
-        <div className="flex flex-col gap-7 text-center text-2xl font-bold">
-          <p>
-            나만 알고싶은 맛집
-            <br />
-            이제는 00에서 함께나눠요
-          </p>
+      <div className="mx-auto flex h-full min-h-screen w-full flex-col items-center justify-center gap-12 px-4 py-10 pt-[32px] tablet:px-[75px] desktop:flex-row desktop:px-4">
+        <div className="text-medium flex flex-col gap-4 text-center">
           <Link href="/" scroll>
             <Image
               src="/images/img_login.png"
-              width={300}
-              height={280}
-              className="w-[290px] tablet:w-[410px] desktop:w-[300px]"
+              width={225}
+              height={150}
+              className="w-[225px] tablet:w-[240px] desktop:w-[300px]"
               alt="메인로고"
             />
           </Link>
+          <p>
+            나만 알고싶은 맛집
+            <br />
+            이제는 MNM에서 함께나눠요
+          </p>
         </div>
-        {children}
+        <div className="flex w-full flex-col items-center justify-center gap-8 rounded-3xl bg-white px-4 py-8 shadow-[0_0_15px_0_rgba(0,0,0,0.05),0_-4px_15px_0_rgba(0,0,0,0.05)] tablet:px-[54px] desktop:w-[510px] desktop:px-[54px]">
+          {children}
+          <div className="flex justify-center gap-4">
+            <Link
+              href={GOOGLE_AUTH_URL}
+              className="flex h-12 w-12 items-center justify-center rounded-full border border-gray-300"
+            >
+              <Image
+                src="/icons/Ic-Google.svg"
+                width={48}
+                height={48}
+                alt="Google 아이콘"
+                draggable={false}
+              />
+            </Link>
+            <Link
+              href={KAKAO_AUTH_URL}
+              className="flex h-12 w-12 items-center justify-center rounded-full"
+            >
+              <Image
+                src="/icons/Ic-Kakao.svg"
+                width={48}
+                height={48}
+                alt="카카오톡 아이콘"
+                draggable={false}
+              />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
